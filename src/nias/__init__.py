4315db98065da20dfeb97faf6e0ddcb36a3c6cea
Stephan Rave <stephanrave@uni-muenster.de>
2024-03-28 11:31:44 +0100

    add license

--- src/nias/__init__.py
+++ src/nias/__init__.py
@@ -1,1 +1,5 @@
+# This file is part of the NiAS project (https://github.com/nias-project).
+# Copyright NiAS developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
 __version__ = '0.0.1.dev0'
