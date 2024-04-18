6facd870dfa9ef17842f87898d07ffc7a38901a2
Stephan Rave <stephanrave@uni-muenster.de>
2024-04-18 18:16:00 +0200

    algorithms.linalg -> linalg

--- src/nias/algorithms/linalg/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file is part of the NiAS project (https://github.com/nias-project).
-# Copyright NiAS developers and contributors. All rights reserved.
-# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
