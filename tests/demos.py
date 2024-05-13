3b9aaeff42f720e17b51084a739d45ccd273e74c
Stephan Rave <stephanrave@uni-muenster.de>
2024-05-13 15:02:40 +0200

    update demo test runner to use typer

--- tests/demos.py
+++ tests/demos.py
@@ -3,24 +3,39 @@
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
 import importlib
-import pytest
+from pathlib import Path
 
+import pytest
+from typer import Typer
+from typer.testing import CliRunner
 
 DEMO_ARGS = (
-    ('gram_schmidt', []),
-    ('eigs', []),
+    ('gram_schmidt', [10, 5]),
+    ('eigs', [10]),
 )
 
 
-@pytest.fixture(params=DEMO_ARGS)
+runner = CliRunner()
+
+def _demo_ids(demo_args):
+    return ['_'.join([args[0]] + [str(a) for a in args[1]]) for args in demo_args]
+
+
+@pytest.fixture(params=DEMO_ARGS, ids=_demo_ids(DEMO_ARGS))
 def demo_args(request):
     return request.param
 
 
 def test_demos(demo_args):
-    module, args = demo_args
+    script, args = demo_args
 
     # Create a spec for the module
-    module_spec = importlib.util.spec_from_file_location('__main__', f'demos/{module}.py')
-    module = importlib.util.module_from_spec(module_spec)
-    module_spec.loader.exec_module(module)
+    base_path = Path(__file__).parent.parent / 'demos'
+    spec = importlib.util.spec_from_file_location(script, base_path / f'{script}.py')
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    app = Typer()
+    app.command()(module.main)
+    result = runner.invoke(app, [str(arg) for arg in args], catch_exceptions=False)
+    assert result.exit_code == 0
