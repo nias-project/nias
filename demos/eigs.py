e9978a5e90dd0a25cb99bf08ae098704af6e8f20
Stephan Rave <stephanrave@uni-muenster.de>
2024-05-13 15:02:14 +0200

    use typer in demos

--- demos/eigs.py
+++ demos/eigs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # This file is part of the NiAS project (https://github.com/nias-project).
 # Copyright NiAS developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
@@ -5,6 +6,7 @@
 import logging
 
 import numpy as np
+from typer import Argument, run
 
 from nias.base.vectorarrays import HilbertSpaceWithBasisFromProductOperator
 from nias.bindings.numpy.operators import NumpyMatrixOperator
@@ -14,24 +16,30 @@ from nias.linalg.gram_schmidt import gram_schmidt
 
 logging.basicConfig(level=logging.INFO)
 
-DIM = 10
 
-product_op = NumpyMatrixOperator(np.eye(DIM))  # TODO: Euclidean spaces
-space = HilbertSpaceWithBasisFromProductOperator(NumpyVectorSpace(DIM), product_op)
+def main(
+    dim: int = Argument(..., min=1, help='Dimension of vector Sapce'),
+):
+    product_op = NumpyMatrixOperator(np.eye(dim))  # TODO: Euclidean spaces
+    space = HilbertSpaceWithBasisFromProductOperator(NumpyVectorSpace(dim), product_op)
 
-S = np.linspace(1., 10., DIM)
-U = space.random(DIM)
-gram_schmidt(U, space.inner_product, copy=False)
-print(space.inner_product(U, U))
-U = space.to_numpy(U).T
+    S = np.linspace(1., 10., dim)
+    U = space.random(dim)
+    gram_schmidt(U, space.inner_product, copy=False)
+    print(space.inner_product(U, U))
+    U = space.to_numpy(U).T
 
-matrix = U @ np.diag(S) @ U.T
-print(np.linalg.eigh(matrix))
+    matrix = U @ np.diag(S) @ U.T
+    print(np.linalg.eigh(matrix))
 
-op = NumpyMatrixOperator(matrix, space, space)
+    op = NumpyMatrixOperator(matrix, space, space)
 
-evals, _ = eigs(op)
-print('Largest eigenvalues:', evals)
+    evals, _ = eigs(op)
+    print('Largest eigenvalues:', evals)
 
-evals, _ = eigs(op, sigma=0.)
-print('Smallest eigenvalues:', evals)
+    evals, _ = eigs(op, sigma=0.)
+    print('Smallest eigenvalues:', evals)
+
+
+if __name__ == '__main__':
+    run(main)
--- demos/gram_schmidt.py
+++ demos/gram_schmidt.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # This file is part of the NiAS project (https://github.com/nias-project).
 # Copyright NiAS developers and contributors. All rights reserved.
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
@@ -5,6 +6,7 @@
 import logging
 
 import numpy as np
+from typer import Argument, run
 
 from nias.bindings.numpy.operators import NumpyMatrixOperator
 from nias.bindings.numpy.vectorarrays import NumpyVectorSpace
@@ -12,12 +14,22 @@ from nias.linalg.gram_schmidt import gram_schmidt
 
 logging.basicConfig(level=logging.INFO)
 
-product_op = NumpyMatrixOperator(np.eye(10))
-product = product_op.as_inner_product()
-space = NumpyVectorSpace(10)
 
-U = space.random(5)
-print(product(U, U))
+def main(
+    dim: int = Argument(..., min=1, help='Dimension of vector Sapce'),
+    num_vecs: int = Argument(..., min=1, help='Number of vectors to orthonormalize'),
+):
+    """Gram-Schmidt orthonormalization of a random VectorArray."""
+    product_op = NumpyMatrixOperator(np.eye(dim))
+    product = product_op.as_inner_product()
+    space = NumpyVectorSpace(dim)
 
-gram_schmidt(U, product, copy=False)
-print(product(U, U))
+    U = space.random(num_vecs)
+    print(product(U, U))
+
+    gram_schmidt(U, product, copy=False)
+    print(product(U, U))
+
+
+if __name__ == '__main__':
+    run(main)
