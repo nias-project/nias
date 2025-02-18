d101065592d045d0ff99b68b172dae39243497c3
Tobias Leibner <7058290+tobiasleibner@users.noreply.github.com>
2025-02-18 11:39:54 +0100

    Inner product bindings for C++ inner product

--- src/nias/bindings/nias_cpp/product.py
+++ src/nias/bindings/nias_cpp/product.py
@@ -1,15 +1,11 @@
-import numpy as np
-from nias.interfaces import InnerProduct, VectorArray
 from numpy.typing import NDArray
 
+from nias.interfaces import InnerProduct, VectorArray
+
+
 class NiasCppInnerProduct(InnerProduct):
+    def __init__(self, impl):
+        self.impl = impl
+
     def apply(self, left: VectorArray, right: VectorArray, pairwise: bool = False) -> NDArray:
-        if pairwise:
-            ret = [left_vec.dot(right_vec) for left_vec, right_vec in zip(left.vectors, right.vectors)]
-            return np.array([ret])
-        else:
-            ret = np.zeros(shape=(len(left), len(right)), dtype=left.scalar_type)
-            for i, left_vec in enumerate(left.vectors):
-                for j, right_vec in enumerate(right.vectors):
-                    ret[i, j] = left_vec.dot(right_vec)
-            return ret
+        return self.impl.apply(left.impl.impl, right.impl.impl, pairwise, left.ind, right.ind)
